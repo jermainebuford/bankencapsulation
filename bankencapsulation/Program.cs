c241bf2db503a81a43ddb1b2ac46af93c623e290
jermaine buford <bufordjermaine@outlook.com>
2022-06-13 15:24:43 -0400

    first commit

---
+++ bankencapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bankencapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc1 = new bankaccount();
+
+            acc1.deposit(100.50);            
+
+            acc1.withdraw(50.50);
+
+            var balance = acc1.getbalance();
+
+            Console.WriteLine(balance);
+        }
+    }
+}
---
+++ bankencapsulation/bankaccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankencapsulation
+{
+    public class bankaccount
+    {
+        private double balance = 0;
+        
+            
+        
+        public void deposit (double amount)
+        {
+            balance += amount; 
+        }
+        public double getbalance ()
+        {
+            return balance;
+        }
+        public void withdraw (double amount)
+        {
+            balance -= amount;
+        }
+    }
+}
